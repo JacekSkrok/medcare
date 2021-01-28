2f6ce90ca00fa9dd8187639762324432b7c2ea63
LazyJackyboy <skrok.jacek21@gmail.com>
2021-01-28 23:50:21 +0100

    done some job with css user-home page and menu

--- medcarapp/src/app/calendar/calendar.page.ts
+++ medcarapp/src/app/calendar/calendar.page.ts
@@ -1,26 +1,24 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 import { CalendarComponent } from 'ionic2-calendar';
 import { Observable } from 'rxjs';
 
 import { Visit } from '../model/Visit';
 
-import { FirebaseService } from '../services/firebase.service'
-import { UserService } from '../services/user.service';
-
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.page.html',
   styleUrls: ['./calendar.page.scss'],
 })
-export class CalendarPage implements OnInit {
+export class CalendarPage{
   visits: Observable<Visit[]>;
 
   eventSource = [];
   viewTitle: string;
 
   calendar = {
-    mode: 'week',
+    mode: 'month',
     currentDate: new Date(),
   };
 
@@ -28,15 +26,18 @@ export class CalendarPage implements OnInit {
 
   @ViewChild(CalendarComponent) myCal: CalendarComponent;
 
-  constructor(  private fbService: FirebaseService,
-                private userService: UserService ) { }
-
- 
-  ngOnInit(): void {
-    this.visits = this.fbService.getVisits();
-    this.createVisitsFromFirebase;
-
-  }
+  constructor(private afs: AngularFirestore) {
+    this.afs.collection('visits').snapshotChanges().subscribe(colSnap => {
+      this.eventSource = [];
+      colSnap.forEach( snap => {
+        const event: any = snap.payload.doc.data();
+        event.id = snap.payload.doc.id;
+        event.endTime = event.endTime.toDate();
+        event.startTime = event.startTime.toDate();
+        this.eventSource.push(event);
+      });
+    });
+   }
 
   addEvent() {
     let start = new Date();
@@ -52,7 +53,7 @@ export class CalendarPage implements OnInit {
     }
 
 
-    this.fbService.addVisit(event);
+    //this.fbService.addVisit(event);
   }
 
   next() {
